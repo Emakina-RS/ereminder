c3ba3c68babc22f3a4431ea2c85d6aca11cc7f1a
Djordje Ljubicic <dj.ljubicic@emakina.rs>
2019-10-04 21:37:51 +0200

    Initaial header css

--- ereminder.web/src/components/Navbar.js
+++ ereminder.web/src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Navbar.css";
+import logo from "../assets/icon/logo.svg";
 
 const Navbar = () => (
-  <div>
-    <a href="">Povratak na sajt</a>
-    <a href="">Izloguj se</a>
+  <div className="header">
+    <img src={logo} className="header-logo" alt="logo" />
+    <div className="links">
+      <ul>
+        <li>
+          <a href="">Povratak na sajt</a>
+        </li>
+        <li>
+          <a href="">Izloguj se</a>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
