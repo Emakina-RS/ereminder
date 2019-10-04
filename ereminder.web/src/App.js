13027e02460f42ce6506a02691fd70f7ae7f590d
Bojan Čoka <coka@fastmail.com>
2019-10-04 19:01:36 +0100

    Provide a starting point for styling

--- ereminder.web/src/App.js
+++ ereminder.web/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Input from "./components/Input";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Input placeholder="Unesite vašu e-mail adresu" />
+      <Input placeholder="Unesite vašu šifru" />
+      <Input placeholder="Ponovite vašu šifru" />
+      <button>Registruj se</button>
+      <span>
+        Već imate nalog? Uloguj se <a href="">ovde</a>
+      </span>
     </div>
   );
 }
---
+++ ereminder.web/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navbar = () => (
+  <div>
+    <a href="">Povratak na sajt</a>
+    <a href="">Izloguj se</a>
+  </div>
+);
+
+export default Navbar;
