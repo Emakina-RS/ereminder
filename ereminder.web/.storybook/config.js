a1617e914cfa1dab0b0dac8ba9eab15f4b4a695a
Bojan Čoka <coka@fastmail.com>
2019-10-05 20:54:29 +0200

    Clean up CRA and Storybook cruft

--- ereminder.web/.storybook/config.js
+++ ereminder.web/.storybook/config.js
@@ -1,5 +1,4 @@
 import { configure } from "@storybook/react";
 import "../src/index.css";
 
-// automatically import all files ending in *.stories.js
 configure(require.context("../src", true, /\.stories\.js$/), module);
--- ereminder.web/src/index.js
+++ ereminder.web/src/index.js
@@ -8,7 +8,6 @@ import "./index.css";
 import login from "./reducers/login";
 import register from "./reducers/register";
 import token from "./reducers/token";
-import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(
   combineReducers({
@@ -25,11 +24,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 if (process.env.NODE_ENV === "development" && module.hot) {
   module.hot.accept("./App", () => {
