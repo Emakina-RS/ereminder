60f17bdb3f28277c886a6546ce11e363edf0491a
Dejan Ostojic <dejan@emakina.rs>
2019-10-05 20:00:55 +0200

    Configured CORS.

--- ereminder.api/server.js
+++ ereminder.api/server.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var cors = require('cors');
 const express = require('express');
 const dbIntializer = require('./dbInitializer');
 const schedule = require('./core/scheduler.js');
@@ -7,19 +8,23 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
 const authenticationHelper = require('./helpers/authenticationHelper');
 
+const config = require('./config/config');
+const corsUrls = global.globalConfig.corsUrls;
+
 schedule.frequentReminderJob();
 schedule.dailyReminderJob();
 
 var app = express()
     .use(bodyParser.urlencoded({ extended: true }))
-    .use(bodyParser.json());
+    .use(bodyParser.json())
+    .use(cors(corsUrls));
 
 authenticationHelper(app);
 routes(app);
 
-var server = app.listen(8081, function () {
+var server = app.listen(global.globalConfig.apiPort, function () {
     var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", 'dev.ereminder.com', port);
+    console.log("Example app listening at http://%s:%s", global.globalConfig.apiUrl, port);
 });
 
 dbIntializer.initializeDB(server);
