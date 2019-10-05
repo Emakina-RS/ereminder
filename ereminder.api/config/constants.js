a2606139636c2efd0213346f8ea75bdfc14998cf
Dejan Ostojic <dejan@emakina.rs>
2019-10-05 16:02:56 +0200

    Added interval and type enums.

--- ereminder.api/config/constants.js
+++ ereminder.api/config/constants.js
@@ -18,5 +18,25 @@ const Constants = {
     }
 }
 
+const NotificationType = {
+    Medicine: 1,
+    Recepies: 2,
+    Pharmacy: 3,
+    Referral: 4
+}
+
+const NotificationInterval = {
+    TwelveHours: 1,
+    TwentyFourHours: 2,
+    OneMonth: 3,
+    TwoMonths: 4,
+    ThreeMonths: 5,
+    FourMonths: 6,
+    FiveMonths: 7,
+    SixMonths: 8,
+    TwelveMonths: 9
+}
 
 module.exports = Constants;
+module.exports.NotificationType = NotificationType;
+module.exports.NotificationInterval = NotificationInterval;
