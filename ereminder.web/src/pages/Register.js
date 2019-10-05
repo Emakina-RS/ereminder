4f19430f192ad6656ba6a237903980108a175a4f
Bojan Čoka <coka@fastmail.com>
2019-10-05 16:58:44 +0200

    Implement basic registration

--- ereminder.web/src/pages/Register.js
+++ ereminder.web/src/pages/Register.js
@@ -1,29 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import Input from "../components/Input";
 import "./Register.css";
 
-const Register = ({ history }) => (
-  <div className="Register">
-    <div className="Register-content">
-      <h1>Registruj se</h1>
-      <Input placeholder="Unesite Vašu e-mail adresu" />
-      <Input placeholder="Unesite šifru" />
-      <Input placeholder="Ponovite šifru" />
-      <Button onClick={() => history.push("/new-reminder")}>
-        Registruj se
-      </Button>
-      <div className="Register-allready">
-        <h2>
-          Već imate nalog? Uloguj se{" "}
-          <Link className="Register-span-link" to="/login">
-            ovde
-          </Link>
-        </h2>
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmpassword] = useState("");
+
+  return (
+    <div className="Register">
+      <div className="Register-content">
+        <h1>Registruj se</h1>
+        <form
+          onSubmit={async event => {
+            event.preventDefault();
+            const response = await fetch("/register", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+                email,
+                password,
+                confirmpassword: confirmPassword
+              })
+            });
+            console.log(response);
+          }}
+        >
+          <Input
+            name="email"
+            onChange={event => setEmail(event.target.value)}
+            placeholder="Unesite Vašu e-mail adresu"
+            type="email"
+            value={email}
+          />
+          <Input
+            name="password"
+            onChange={event => setPassword(event.target.value)}
+            placeholder="Unesite šifru"
+            type="password"
+            value={password}
+          />
+          <Input
+            name="confirm-password"
+            onChange={event => setConfirmpassword(event.target.value)}
+            placeholder="Ponovite šifru"
+            type="password"
+            value={confirmPassword}
+          />
+          <Button>Registruj se</Button>
+          <div className="Register-allready">
+            <h2>
+              Već imate nalog? Uloguj se{" "}
+              <Link className="Register-span-link" to="/login">
+                ovde
+              </Link>
+            </h2>
+          </div>
+        </form>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Register;
