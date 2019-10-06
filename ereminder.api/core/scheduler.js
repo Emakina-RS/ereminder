8f2ddc61687b91289cd11f92a71993b0603719f8
Dejan Ostojic <dejan@emakina.rs>
2019-10-06 15:01:08 +0200

    Implemented email notification scheduler.

--- ereminder.api/core/scheduler.js
+++ ereminder.api/core/scheduler.js
@@ -4,20 +4,9 @@ const cron = require('node-cron');
 const notificationService = require('../services/notificationService');
 
 exports.initialize = function() {
-    frequentReminderJob();
-    dailyReminderJob();
-}
-
-function frequentReminderJob() {
     cron.schedule("*/3 * * * *", function() {
         console.log("running a task every 3 minutes");
-        notificationService.sendFrequentNotifications();
-    });
-};
 
-function dailyReminderJob() {
-    cron.schedule("* 10 * * *", function() {
-        console.log("running a task every day at 10 AM");
-        notificationService.sendNonFrequentNotifications();
+        notificationService.sendEmailNotifications();
     });
 };
