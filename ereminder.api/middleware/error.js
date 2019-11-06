ba490210db111feca21d9380828e52424bc457d1
Dejan Ostojic <dejan@emakina.rs>
2019-11-06 19:31:05 +0100

    Added handling BadInputError

--- ereminder.api/middleware/error.js
+++ ereminder.api/middleware/error.js
@@ -1,11 +1,15 @@
 "use strict";
 
 const logger = require("../startup/logger")();
+const BadInputError = require("../errors/BadInputError");
 
 module.exports = function(err, req, res, next) {
   logger.error(`[Middleware:Error] :  ${err.message}. \n ${err.stack}`);
 
-  //TODO: check the error type, if it's of BadInputError then reteurn 400 (bad request), instead of 500
+  if (err instanceof BadInputError) {
+    res.status(400).json({ message: "Invalid input." });
+    return;
+  }
 
   res.status(500).json({ message: "Something failed." });
 };
